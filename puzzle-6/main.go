874e939fa757ae87b2bc4d6818747b80167ddf05
Simon Breitfelder <sbreitf1@web.de>
2025-12-08 13:50:49 +0100

    puzzle-6, part-2

--- puzzle-6/main.go
+++ puzzle-6/main.go
@@ -5,20 +5,85 @@ package main
 import (
 	"aoc/helper"
 	"fmt"
-	"regexp"
 	"strings"
 )
 
 func main() {
 	lines := helper.ReadNonEmptyLines("input.txt")
 
-	problems := ParseProblems(lines)
+	grid := ReadGrid(lines)
 
-	solution1 := SumResults(problems)
+	solution1 := SumResults(ParseProblems1(grid))
 	fmt.Println("-> part 1:", solution1)
 
-	solution2 := 0
+	solution2 := SumResults(ParseProblems2(grid))
 	fmt.Println("-> part 2:", solution2)
+}
+
+func ReadGrid(lines []string) [][]string {
+	lineRunes := helper.LinesToRunes(lines)
+
+	rows := make([][]string, len(lineRunes))
+	colStart := 0
+Outter:
+	for i := 0; i <= len(lineRunes[0]); i++ {
+		if i < len(lineRunes[0]) {
+			for j := 0; j < len(lineRunes); j++ {
+				if lineRunes[j][i] != ' ' {
+					continue Outter
+				}
+			}
+		}
+
+		for j := 0; j < len(lineRunes); j++ {
+			rows[j] = append(rows[j], string(lineRunes[j][colStart:i]))
+		}
+		colStart = i + 1
+	}
+	return rows
+}
+
+func ParseProblems1(grid [][]string) []Problem {
+	problems := make([]Problem, 0, len(grid[0]))
+	for i := 0; i < len(grid[0]); i++ {
+		nums := make([]int64, len(grid)-1)
+		for j := 0; j < len(nums); j++ {
+			nums[j] = helper.ParseInt[int64](strings.TrimSpace(grid[j][i]))
+		}
+		op := strings.TrimSpace(grid[len(grid)-1][i])
+		problems = append(problems, Problem{
+			Numbers:  nums,
+			Operator: op,
+		})
+	}
+	return problems
+}
+
+func ParseProblems2(grid [][]string) []Problem {
+	problems := make([]Problem, 0, len(grid[0]))
+	for i := 0; i < len(grid[0]); i++ {
+		numStrs := make([]string, len(grid[0][i]))
+		for j := 0; j < len(numStrs); j++ {
+			for k := 0; k < len(grid)-1; k++ {
+				r := []rune(grid[k][i])[j]
+				if r >= '0' && r <= '9' {
+					numStrs[j] = numStrs[j] + string(r)
+				}
+			}
+		}
+
+		nums := make([]int64, len(numStrs))
+		for j := 0; j < len(numStrs); j++ {
+			nums[j] = helper.ParseInt[int64](strings.TrimSpace(numStrs[j]))
+		}
+
+		op := strings.TrimSpace(grid[len(grid)-1][i])
+		problems = append(problems, Problem{
+			Numbers:  nums,
+			Operator: op,
+		})
+	}
+	return problems
 }
 
 type Problem struct {
@@ -46,33 +111,6 @@ func (p Problem) Calc() int64 {
 		helper.ExitWithMessage("operator %q unknown", p.Operator)
 		return 0
 	}
-}
-
-func ParseProblems(lines []string) []Problem {
-	splitPattern := regexp.MustCompile(`\s+`)
-	lineParts := make([][]string, len(lines))
-	for i := 0; i < len(lines); i++ {
-		lineParts[i] = splitPattern.Split(strings.TrimSpace(lines[i]), -1)
-		if i > 0 && len(lineParts[i]) != len(lineParts[0]) {
-			helper.ExitWithMessage("lines have different number of operands")
-		}
-	}
-
-	numCount := len(lineParts) - 1
-	problemCount := len(lineParts[0])
-
-	problems := make([]Problem, problemCount)
-	for i := 0; i < problemCount; i++ {
-		nums := make([]int64, numCount)
-		for j := 0; j < numCount; j++ {
-			nums[j] = helper.ParseInt[int64](lineParts[j][i])
-		}
-		problems[i] = Problem{
-			Numbers:  nums,
-			Operator: lineParts[numCount][i],
-		}
-	}
-	return problems
 }
 
 func SumResults(problems []Problem) int64 {
